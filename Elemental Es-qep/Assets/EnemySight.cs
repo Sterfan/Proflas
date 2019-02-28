cc10818265bec0e40d89a7213947e2d26cdc8685
Maherj <Maherj.jaber23@gmail.com>
2019-02-28 12:56:51 +0100

    v0.5.7

---
+++ Elemental Es-qep/Assets/EnemySight.cs
@@ -0,0 +1,28 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class EnemySight : MonoBehaviour
+//{
+//    [SerializeField]
+//    private BossMeleeArm enemy;
+
+
+//   void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.tag=="Player")
+//        {
+//            enemy.target = other.gameObject;
+//            Debug.Log("EnemyInSight!");
+//        }
+//    }
+
+//    void OnTriggerExit2D (Collider2D other)
+//    {
+//        if (other.tag == "Player")
+//        {
+//            enemy.target = null;
+//        }
+//    }
+
+//}
