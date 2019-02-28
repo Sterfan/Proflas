531ad48bccc9fba5b2c007a0a8b73e0353c1aedf
Maherj <Maherj.jaber23@gmail.com>
2019-02-28 13:00:07 +0100

    try

--- Elemental Es-qep/Assets/Scripts/newScripts/BossMeleeArm.cs
+++ Elemental Es-qep/Assets/Scripts/newScripts/BossMeleeArm.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BossMeleeArm : MonoBehaviour
 {
     public Transform castPoint;
-    private Vector2 dir = new Vector2(0, -1);
+    private Vector2 dir = new Vector2(0, 1);
     public GameObject Bulletprefab;
-    private float range = 10f;
+    private float range = 8f;
     private float speed = 2;
     private float ShootingDelay = 2f;
     private float cooldownTimer = 2.5f;
